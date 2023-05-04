85974a070ed0fa580522e9c989e4e34c395ec03a
V.M. Gadzhiev <vadingg@mail.ru>
2023-05-04 16:22:16 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
             System.out.println("i = " + 2);
+            System.out.println("1");
         }
     }
 }
